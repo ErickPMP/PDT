e17f54aec13a159b7bec467ab5c47fa560d42dc9
ErickPMP <123596392+ErickPMP@users.noreply.github.com>
2023-01-31 18:41:58 -0500

    Update Control-Proporcional.ino

--- Control-Proporcional.ino
+++ Control-Proporcional.ino
@@ -1,4 +1,3 @@
-// C++ code
 #define PI 3.1415926535897932384626433832795
 #include <Wire.h>
 #include "SDL_Arduino_INA3221.h"
@@ -18,27 +17,26 @@ int D9=9;
 int D11=11;
 int duty1=25;
 int duty2=25;
-float mil,sec=0,t,In, Out, rk, rk1;
+float mil,sec=0,t;
 float Out1,Out2,Out1a,Out1b,Out2a,Out2b;
-float e,u,SPo,Kp=5.0,SensorC=0;
+float e,u,un,en,SPo,Kp=20,SensorC=0;
 float shuntvoltage1 = 0;
-  float busvoltage1 = 0;
-  float current_mA1 = 0;
-  float loadvoltage1 = 0;
-float b0;
-float b1;
-float a1;
-
-
+float busvoltage1 = 0;
+float current_mA1 = 0;
+float loadvoltage1 = 0;
 
 void setup(){
-  Serial.begin(9600);
+  Serial.begin(19200);
   pinMode(D10, OUTPUT);
   pinMode(D4, OUTPUT);
   pinMode(D3, OUTPUT);
   pinMode(D5, OUTPUT);
   pinMode(D9, OUTPUT);
   pinMode(D11, OUTPUT);
+  
+  //**********************Leyendas*********************
+  Serial.println("u,e,SPo,SensorC");
+  //*************************************************** IMPORTANTE NO MOVER O HAY CONFLICTO CON EL INA3221
   
   noInterrupts();           // deshabilita las interrupciones
 
@@ -66,15 +64,13 @@ void setup(){
   interrupts();
 
   //******************************* INA3221*****************************
-  Serial.println("SDA_Arduino_INA3221_Test");
-  
-  Serial.println("Measuring voltage and current with ina3221 ...");
+  //Serial.println("SDA_Arduino_INA3221_Test");
+  //Serial.println("Measuring voltage and current with ina3221 ...");
   ina3221.begin();
-
-  Serial.print("Manufactures ID=0x");
+  //Serial.print("Manufactures ID=0x");
   int MID;
   MID = ina3221.getManufID();
-  Serial.println(MID,HEX);
+  //Serial.println(MID,HEX);
   //********************************************************************
 }
 
@@ -86,35 +82,32 @@ ISR(TIMER0_COMPA_vect){          // timer 0  (cada 1 mS)
   }
  t= sec+mil/1000.0;
 
- //Out = rk*b0 + rk1*b1;
- //rk = In - rk1*a1;
- //rk1 = rk;
- 
-
- Out1 = 255.0*sin(2.0*PI*0.2*t);     // SENO para EL PIN D3, D4 y D5
+ //Out1 = 255.0*sin(2.0*PI*0.2*t);     // SENO para EL PIN D3, D4 y D5
  Out2 = 255.0*sin(2.0*PI*0.2*t);    // SENO para EL PIN D11, D10 y D9
 
- if (Out1 >= 0.0) {  Out1a = Out1;digitalWrite(D4,HIGH);digitalWrite(D5,LOW);OCR2B  =  Out1a; flag = 1;} // D4 y D5 Compuerta AND seno POSITIVO
-else { Out1b = abs(Out1);digitalWrite(D4,LOW);digitalWrite(D5,HIGH);OCR2B  =  abs(Out1); flag = 0;} // D4 y D5 Compuerta AND seno NEGATIVO
-
-if (Out2 >= 0.0) {  Out2a = Out2;digitalWrite(D10,HIGH);digitalWrite(D9,LOW);OCR2A=Out2a;} // D10 y D9 Compuerta AND seno POSITIVO
-else { Out2b = abs(Out2);digitalWrite(D10,LOW);digitalWrite(D9,HIGH);OCR2A=abs(Out2);} // D10 y 9 Compuerta AND seno NEGATIVO
-/*
-SPo=100.0*sin(2.0*PI*0.2*t);
+SPo=180.0*sin(2.0*PI*0.2*t);
 e=SPo-SensorC;
+if(e<0){en=e;e=-e;}
+else{en=e;e=e;}
 u=Kp*e;
-u=constrain(u,0,255);
-Out1=u;
-*/
+u=constrain(u,0,254);
+//Out1=u;
+
+if (SPo >= 0.0) {  un = u;digitalWrite(D4,HIGH);digitalWrite(D5,LOW);OCR2A  =  u;SensorC=current_mA1;} // D4 y D5 Compuerta AND seno POSITIVO
+else { un = -u;digitalWrite(D4,LOW);digitalWrite(D5,HIGH);OCR2A  =  abs(u);SensorC=-current_mA1;} // D4 y D5 Compuerta AND seno NEGATIVO
+
+if (SPo >= 0.0) {  Out2a = Out2;digitalWrite(D10,HIGH);digitalWrite(D9,LOW);OCR2B=Out2;} // D10 y D9 Compuerta AND seno POSITIVO
+else { Out2b = abs(Out2);digitalWrite(D10,LOW);digitalWrite(D9,HIGH);OCR2B=abs(Out2);} // D10 y 9 Compuerta AND seno NEGATIVO
+
 }
 
 void loop() {
-  
+  //*********Lectura del A0**********
   float sen=analogRead(A0);
-  double senmap=5.0*sen/1024.0;
-//Serial.println(senmap);
+  double senmap=5.0*sen/1024.0; //Conversion 1024 a 5V
+ //Serial.println(senmap);
  //Serial.print(",");
- //****************
+ //**********************************
  /*
  Serial.print(Out1a);
  Serial.print(",");
@@ -125,47 +118,24 @@ void loop() {
 //**********************************************************************
 
 //*************************INA3221*************************************
-//Serial.println("------------------------------");
-  
-
 
   busvoltage1 = ina3221.getBusVoltage_V(LIPO_BATTERY_CHANNEL);
   shuntvoltage1 = ina3221.getShuntVoltage_mV(LIPO_BATTERY_CHANNEL);
-  current_mA1 = -ina3221.getCurrent_mA(LIPO_BATTERY_CHANNEL);  // minus is to get the "sense" right.   - means the battery is charging, + that it is discharging
-  loadvoltage1 = busvoltage1 + (shuntvoltage1 / 1000);
-  
-  if(flag == true)
-  {
-    SensorC=current_mA1;
-  }
-  else if(flag == false)
-  {
-    SensorC=-1*current_mA1;
-  }
-    
+  current_mA1 = ina3221.getCurrent_mA(LIPO_BATTERY_CHANNEL);  // minus is to get the "sense" right.   - means the battery is charging, + that it is discharging
+  loadvoltage1 = busvoltage1 + (shuntvoltage1 / 1000);  
     //Serial.print("LIPO_Battery Bus Voltage:   "); Serial.print(busvoltage1); Serial.println(" V");
     //Serial.print("LIPO_Battery Shunt Voltage: "); Serial.print(shuntvoltage1); Serial.println(" mV");
     //Serial.print("LIPO_Battery Load Voltage:  "); Serial.print(loadvoltage1); Serial.println(" V");
 //Serial.print("LIPO_Battery Current 1:       "); Serial.print(current_mA1); Serial.print(" mA");
 //Serial.print(",");
  //*******************************************************************
-/*
-Serial.print(u);
+
+Serial.print(un);
 Serial.print(",");
-Serial.print(e);
+Serial.print(en);
 Serial.print(",");
-*/
-Serial.print(Out1);
+Serial.print(SPo);
 Serial.print(",");
 Serial.println(SensorC);
-/*
-if(flag == 1)
-{
-  Serial.println(current_mA1);
-}
-else if(flag ==0)
-{
-  Serial.println(-1*current_mA1);
-}
-*/
+
 }
